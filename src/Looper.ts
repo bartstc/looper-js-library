7945526c79564661d6571f1274479bfa412b37e8
Bartosz Stolc <bartst010395@gmail.com>
2019-09-22 00:01:31 +0200

    Jest setup, testing Looper class

--- src/Looper.ts
+++ src/Looper.ts
@@ -1,9 +1,14 @@
+/* Most of the functions comes from Underscore.js library.
+  [REMAKE] - my own implementation of functions from Underscore.js
+  [CUSTOM] - made by me
+*/
+
 interface Map<T> {
   [key: string]: T;
 }
 
 export class Looper {
-  // Returns the first element of an array. Passing n will return the first n elements of the array.
+  // [REMAKE] Returns the first element of an array. Passing n will return the first n elements of the array.
   first<T>(arr: T[], amount?: number): T | T[] {
     if (amount) {
       return arr.filter((_, i) => i < amount);
@@ -12,7 +17,7 @@ export class Looper {
     return arr[0];
   }
 
-  // Returns the last element of an array. Passing n will return the last n elements of the array.
+  // [REMAKE] Returns the last element of an array. Passing n will return the last n elements of the array.
   last<T>(arr: T[], amount?: number): T | T[] {
     if (amount) {
       return arr.filter((_, i) => i >= arr.length - amount);
@@ -21,8 +26,12 @@ export class Looper {
     return arr[arr.length - 1];
   }
 
-  // Flattens a nested array (the nesting can be to any depth). If you pass shallow, the array will only be flattened a single level.
+  // [REMAKE] Flattens a nested array (the nesting can be to any depth). If you pass shallow, the array will only be flattened a single level.
   flatten(arr: any[], shallow: boolean = false): any[] {
+    //  ES2019 implementation
+    //  if (shallow) return arr.flat();
+    //  return arr.flat(Infinity);
+
     return arr.reduce((flat, toFlatten, _, __) => {
       if (shallow) return flat.concat(toFlatten);
 
@@ -31,12 +40,8 @@ export class Looper {
       );
     }, []);
   }
-  // flatten(arr: any[], shallow: boolean = false): any[] {
-  //   if (shallow) return arr.flat();
-  //   return arr.flat(Infinity);
-  // }
 
-  // Returns a copy of the array with all instances of the values removed.
+  // [REMAKE] Returns a copy of the array with all instances of the values removed.
   without<T>(arr: T[], ...valuesToRemove: T[]): T[] {
     return arr.filter(item => {
       const found = valuesToRemove.includes(item);
@@ -45,40 +50,34 @@ export class Looper {
     });
   }
 
-  // Returns new array with values that shows up in both arrays.
+  // [CUSTOM] Returns new array with values that shows up in both arrays.
   same<T>(firstArr: T[], secondArr: T[]): any[] {
-    return firstArr.filter(item => {
-      const found = secondArr.includes(item);
-
-      if (found) return item;
-    });
+    return firstArr.filter(item => secondArr.includes(item));
   }
 
-  // Returns the values from array that are not present in the other arrays.
+  // [REMAKE] Returns the values from array that are not present in the other arrays.
   difference<T>(firstArr: T[], secondArr: T[]): any[] {
-    return firstArr.filter(item => {
-      const found = secondArr.includes(item);
-
-      if (!found) return item;
-    });
+    return firstArr
+      .filter(item => !secondArr.includes(item))
+      .concat(secondArr.filter(item => !firstArr.includes(item)));
   }
 
-  // Returns new merged array.
+  // [CUSTOM] Returns new merged array.
   merge(...arraysToMerge: any[]): any[] {
-    return [].concat(...arraysToMerge);
+    return arraysToMerge.length === 0 ? [] : [].concat(...arraysToMerge);
   }
 
-  // Returns new array with unique values.
+  // [CUSTOM] Returns new array with unique values.
   unique<T>(arr: T[]): T[] {
     return [...new Set(arr)];
   }
 
-  // Looks through each array, returning new array with unique values.
+  // [CUSTOM] Looks through each array, returning new array with unique values.
   uniqueAll(...arrays: any[]): any[] {
-    return [...new Set(this.merge(...arrays))];
+    return arrays.length === 0 ? [] : [...new Set(this.merge(...arrays))];
   }
 
-  // Convert array into map.
+  // [CUSTOM] Convert array into map.
   toMap<T>(arr: T[]): Map<T> {
     return arr.reduce(
       (acc, item, i, _): Map<T> => ({
@@ -88,7 +87,4 @@ export class Looper {
       {}
     );
   }
-
-  // Converts arrays into objects. Pass either a single list of [key, value] pairs, or a list of keys, and a list of values. Passing by pairs is the reverse of pairs. If duplicate keys exist, the last value wins.
-  // object(firstArr: any[] | any[][], secondArray?: any[]): Map<any> {}
 }
